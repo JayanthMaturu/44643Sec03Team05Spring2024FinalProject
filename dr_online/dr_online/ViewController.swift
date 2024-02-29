c5db5d735229b76b99098693694b9a4b27d19f6b
JayanthMaturu <143036932+JayanthMaturu@users.noreply.github.com>
2024-02-28 22:26:31 -0600

    Jayanth: resolved conflicts of jahnavi and jayanth branch

--- dr_online/dr_online/ViewController.swift
+++ dr_online/dr_online/ViewController.swift
@@ -6,9 +6,21 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
+        didSet{
+            LaunchLAV.animation = LottieAnimation.named("Animation_app")
+            LaunchLAV.alpha = 1
+            LaunchLAV.play{ [weak self] _ in
+                       UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0, options: [.curveEaseInOut]){
+                            self?.LaunchLAV.alpha = 0.0
+                        }
+                    }
+                }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
