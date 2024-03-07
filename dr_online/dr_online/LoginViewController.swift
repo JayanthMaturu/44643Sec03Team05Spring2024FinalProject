1c11a6feafa48ffe657712cdb1de7187ccb7b717
jahnavichava <jahnavichava42@gmail.com>
2024-03-06 20:01:22 -0600

    outlets are created and writing the functionality in login page

--- dr_online/dr_online/LoginViewController.swift
+++ dr_online/dr_online/LoginViewController.swift
@@ -9,23 +9,53 @@ import UIKit
 import Lottie
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var LaunchLAV: LottieAnimationView!{
         didSet{
             LaunchLAV.animation = LottieAnimation.named("Animation_app")
             LaunchLAV.alpha = 1
             LaunchLAV.play{ [weak self] _ in
-                       UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0, options: [.curveEaseInOut]){
-                            self?.LaunchLAV.alpha = 0.0
-                        }
-                    }
+                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0, options: [.curveEaseInOut]){
+                    self?.LaunchLAV.alpha = 0.0
                 }
+            }
+        }
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var login: UIButton!
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        self.emailTF.text = ""
+        self.passwordTF.text = ""
+        
+        
     }
-
-
-}
+    
+    
+    @IBAction func loginBTN(_ sender: Any) {
+        let email = emailTF.text ?? ""
+        let password = passwordTF.text ?? ""
+        guard !email.isEmpty && !password.isEmpty else {
+            showAlert(message: "Please enter both email and password.")
+            return
+            
+        }
+    }
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+        
+        func showAlert(message: String) {
+            let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
+    }
 
