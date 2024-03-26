a43bcba5150d192c468aae2305eef25cfda0901e
varunreddy272002 <varun.reddy27072000@gmail.com>
2024-03-25 23:00:54 -0500

    working on Apimodel using the urlComponents and jsondata

--- dr_online/dr_online/APIModel.swift
+++ dr_online/dr_online/APIModel.swift
@@ -8,4 +8,27 @@
 import Foundation
 import Alamofire
 
+struct doctor:Codable{
+    var id: Int
+    var Doctors: String
+    var Diseases: String
+}
+struct doctorDeets {
+    //var doctor: item
+}
+struct DataManager {
+    private(set) var doctors = [doctorDeets]()
+    func loadMenu() async{
+    // https://retoolapi.dev/uYiI4p/data
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "retoolapi.dev"
+        urlComponents.path = "uYiI4p/data"
+        
+        let task = AF.request(urlComponents.url!, method: .get).serializingDecodable([doctor].self)
+        let response = await task.response
+        guard let jsonData = response.value else {return}
+        
+    }
+}
 
