815a1517b3ad46e8f45e20b59a524ef6ec947f79
jahnavichava <jahnavichava42@gmail.com>
2024-04-12 22:23:16 -0500

    Upadated the functionality

--- dr_online/dr_online/reportpageViewController.swift
+++ dr_online/dr_online/reportpageViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import PDFKit
+import MyModule // Replace 'MyModule' with the correct module name
+
+
 
 class reportpageViewController: UIViewController {
     
@@ -34,59 +38,94 @@ class reportpageViewController: UIViewController {
     
     @IBAction func DownLoadBTN(_ sender: UIButton) {
         
-        
+        createPDF()
+
     }
+    var patientData: PatientData?
     
     
-    
-    
-    private func setupUI() {
-        // Create and set up the scroll view
-        scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(scrollView)
+    @objc func downloadReport() {
+        createPDF()
+    }
+    private func createPDF() {
+        let pdf = PDFDocument()
         
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        
-        
-        
-        //    func displayPatientDetails() {
-        //            if let data = patientData {
-        //
-        //                Name: \(data.name)
-        //                Age: \(data.age)
-        //                Gender: \(data.gender)
-        //                Symptoms: \(data.symptoms.joined(separator: ", "))
-        //                Diagnosis: \(data.diagnosis)
-        //                Prescription: \(data.prescription)
-        //                """
-        //            }
-        //    }
-        
-        func configureScrollViewContent() {
-            let labels = ["Name", "Age", "Gender", "Diagnosis", "Prescription"]
-            var currentYPosition: CGFloat = 20
+        // Create a PDF page and add patient details
+        let page = PDFPage()
+        let pageInfo = [
+            "Name": nameTextField.text ?? "",
+            "Age": ageTextField.text ?? "",
+            "Gender": genderTextField.text ?? "",
+            "Diagnosis": diagnosisTextField.text ?? "",
+            "Prescription": prescriptionTextField.text ?? ""
+        ]
+        //private func setupUI() {
+            // Create and set up the scroll view
+            scrollView = UIScrollView()
+            scrollView.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(scrollView)
             
-            for labelText in labels {
-                let label = UILabel(frame: CGRect(x: 20, y: currentYPosition, width: scrollView.bounds.width - 40, height: 30))
-                label.text = labelText
-                scrollView.addSubview(label)
+            NSLayoutConstraint.activate([
+                scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+            
+            configureScrollViewContent()
+                    
+                    // Set up download button
+            DownloadBTN.setTitle("Download Report", for: .normal)
+            DownloadBTN.addTarget(self, action: #selector(ReportPage(_:)), for: .touchUpInside)
+                }
+            
+        func createPDF() {
+               let pdf = PDFDocument()
+               
+               // Create a PDF page and add patient details
+               let page = PDFPage()
+               let pageInfo = [
+                   "Name": nameTextField.text ?? "",
+                   "Age": ageTextField.text ?? "",
+                   "Gender": genderTextField.text ?? "",
+                   "Diagnosis": diagnosisTextField.text ?? "",
+                   "Prescription": prescriptionTextField.text ?? ""
+               ]
+               var pageContent = ""
+               for (key, value) in pageInfo {
+                   pageContent += "\(key): \(value)\n"
+               }
+               page.addAnnotation(PDFAnnotation(bounds: CGRect(x: 20, y: 20, width: 200, height: 200), forType: .text, withProperties: [key: pageContent]))
+               pdf.insert(page, at: pdf.pageCount)
+               
+               // Save or share the PDF document
+               let pdfData = pdf.dataRepresentation()
+               let activityViewController = UIActivityViewController(activityItems: [pdfData], applicationActivities: nil)
+               present(activityViewController, animated: true, completion: nil)
+           }
+       }
+            
+           
+            
+            func configureScrollViewContent() {
+                let labels = ["Name", "Age", "Gender", "Diagnosis", "Prescription"]
+                var currentYPosition: CGFloat = 20
                 
-                currentYPosition += 40
-                
-                let textField = UITextField(frame: CGRect(x: 20, y: currentYPosition, width: scrollView.bounds.width - 40, height: 30))
-                textField.borderStyle = .roundedRect
-                scrollView.addSubview(textField)
-                textField.tag = labels.firstIndex(of: labelText)! + 1
-                
-                currentYPosition += 50
+                for labelText in labels {
+                    let label = UILabel(frame: CGRect(x: 20, y: currentYPosition, width: scrollView.bounds.width - 40, height: 30))
+                    label.text = labelText
+                    scrollView.addSubview(label)
+                    
+                    currentYPosition += 40
+                    
+                    let textField = UITextField(frame: CGRect(x: 20, y: currentYPosition, width: scrollView.bounds.width - 40, height: 30))
+                    textField.borderStyle = .roundedRect
+                    scrollView.addSubview(textField)
+                    textField.tag = labels.firstIndex(of: labelText)! + 1
+                    
+                    currentYPosition += 50
+                }
+                scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: currentYPosition + 20)
             }
-            scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: currentYPosition + 20)
-        }
-    }
-}
+        
+
