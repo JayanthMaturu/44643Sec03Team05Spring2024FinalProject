c8ff06d59ae677e50543a9724eb1a8c913248561
jahnavichava <jahnavichava42@gmail.com>
2024-04-12 21:31:07 -0500

    Update the PatientData struct for report page and modified

--- dr_online/dr_online/reportpageViewController.swift
+++ dr_online/dr_online/reportpageViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class reportpageViewController: UIViewController {
     
+    struct PatientData {
+        var name: String
+        var age: String
+        var gender: String
+        var symptoms: [String]
+        var diagnosis: String
+        var prescription: String
+    }
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,16 +24,51 @@ class reportpageViewController: UIViewController {
     @IBOutlet weak var genderTextField: UITextField!
     @IBOutlet weak var diagnosisTextField: UITextField!
     @IBOutlet weak var prescriptionTextField: UITextField!
-    
+    @IBOutlet weak var DownloadBTN: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    func configureScrollViewContent() {
+    @IBAction func DownLoadBTN(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    
+    private func setupUI() {
+        // Create and set up the scroll view
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        
+        
+        //    func displayPatientDetails() {
+        //            if let data = patientData {
+        //
+        //                Name: \(data.name)
+        //                Age: \(data.age)
+        //                Gender: \(data.gender)
+        //                Symptoms: \(data.symptoms.joined(separator: ", "))
+        //                Diagnosis: \(data.diagnosis)
+        //                Prescription: \(data.prescription)
+        //                """
+        //            }
+        //    }
+        
+        func configureScrollViewContent() {
             let labels = ["Name", "Age", "Gender", "Diagnosis", "Prescription"]
             var currentYPosition: CGFloat = 20
             
@@ -43,6 +86,7 @@ class reportpageViewController: UIViewController {
                 
                 currentYPosition += 50
             }
-                scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: currentYPosition + 20)
+            scrollView.contentSize = CGSize(width: scrollView.bounds.width, height: currentYPosition + 20)
         }
+    }
 }
